1bd4bfbc86a821ee601a5bae751ccc0aab404561
cagan <cagankota@gmail.com>
2023-11-16 01:02:47 +0300

    Added a first check before 3-min interval, more detailed message after got an appointment

--- index.js
+++ index.js
@@ -8,6 +8,43 @@ const iller = require("./iller.json").iller
 
 const tckimlik = prompt("TC kimlik numarası: ")
 const sifre = prompt("Şifre: ")
+
+let interval;
+
+function kontrolEt(token, il, cinsiyet, klinik, onumuzdekigun) {
+   functions.kullaniciRandevulari(token).then(randevular => {
+         if (randevular.aktifRandevuDtoList.filter(a => a.mhrsKlinikAdi == klinik.text).length <= 0) {
+            const date = moment().format('YYYY-MM-DD HH:mm:ss')
+            functions.randevuAra(token, il.plaka, cinsiyet, klinik.value, String(date), String(moment().add(Number(onumuzdekigun), 'days').format('YYYY-MM-DD HH:mm:ss'))).then(veri => {
+               functions.hekimAra(token, il.plaka, cinsiyet, klinik.value, veri.kurumId, veri.hekimId).then(veri => {
+                  const kullanilabilirhekim = veri.filter(hekim => hekim.kalanKullanim > 0)
+                  if (kullanilabilirhekim.length > 0) {
+                     const saatler = kullanilabilirhekim[0].hekimSlotList[0].muayeneYeriSlotList[0].saatSlotList.filter(saat => saat.bos == true)
+                     const slotList = []
+                     saatler.map(saat => {
+                        saat.slotList.map(a => slotList.push(a))
+                     })
+                     
+                     const alinabilir = slotList.filter(a => a.bos == true)
+                     
+                     functions.randevuAl(token, alinabilir[0].slot.id, alinabilir[0].slot.fkCetvelId, alinabilir[0].slot.baslangicZamani, alinabilir[0].slot.bitisZamani)
+                     .then(resp => {
+                        console.log(`Randevu alındı\nHekim adı: ${resp.hekim.ad} ${resp.hekim.soyad}\nKurum adı: ${resp.kurum.kurumAdi} (${resp.kurum.ilAdi}-${resp.kurum.ilceAdi})\nRandevu tarihi: ${resp.randevuBaslangicZamaniStr.zaman} - ${resp.randevuBitisZamaniStr.saat}`)
+                        clearInterval(interval)
+                     }).catch(() => console.error("Randevu alınırken hata oluştu"))
+                  }
+               }).catch(() => console.error("Hekim aranırken hata oluştu"))
+            }).catch(err => {
+               if (err.response.data.errors[0].kodu == "RND4010") console.log("Randevu bulunamadı")
+               else console.error("Randevu bulunurken hata oluştu")
+            })
+         } else {
+            console.log("Zaten randevu alınmış")
+            clearInterval(interval)
+         }
+      }).catch(() => console.error("Randevu geçmişi alınırken hata oluştu"))
+}
+
 functions.girisYap(tckimlik, sifre).then(rawtoken => {
    console.log("Giriş başarılı")
    
@@ -28,34 +65,8 @@ functions.girisYap(tckimlik, sifre).then(rawtoken => {
 
    const token = String("Bearer " + rawtoken.split('"')[1])
    console.log("Başladı, her 3 dakikada bir randevular kontrol edilecek")
-   let interval = setInterval(() => {
-      functions.kullaniciRandevulari(token).then(randevular => {
-         if (randevular.aktifRandevuDtoList.filter(a => a.mhrsKlinikAdi == klinik.text).length <= 0) {
-            const date = moment().format('YYYY-MM-DD HH:mm:ss')
-            functions.randevuAra(token, il.plaka, cinsiyet, klinik.value, String(date), String(moment().add(Number(onumuzdekigun), 'days').format('YYYY-MM-DD HH:mm:ss'))).then(veri => {
-               functions.hekimAra(token, il.plaka, cinsiyet, klinik.value, veri.kurumId, veri.hekimId).then(veri => {
-                  const kullanilabilirhekim = veri.filter(hekim => hekim.kalanKullanim > 0)
-                  if (kullanilabilirhekim.length > 0) {
-                     const saatler = kullanilabilirhekim[0].hekimSlotList[0].muayeneYeriSlotList[0].saatSlotList.filter(saat => saat.bos == true)
-                     const slotList = []
-                     saatler.map(saat => {
-                        saat.slotList.map(a => slotList.push(a))
-                     })
-                     
-                     const alinabilir = slotList.filter(a => a.bos == true)
-                     
-                     functions.randevuAl(token, alinabilir[0].slot.id, alinabilir[0].slot.fkCetvelId, alinabilir[0].slot.baslangicZamani, alinabilir[0].slot.bitisZamani)
-                     .then(resp => {
-                        console.log("randevu alındı")
-                        clearInterval(interval)
-                     }).catch(err => console.error("Randevu alınırken hata oluştu"))
-                  }
-               }).catch(err => console.error("Hekim aranırken hata oluştu"))
-            }).catch(err => {
-               if (err.response.data.errors[0].kodu == "RND4010") console.log("Randevu bulunamadı")
-               else console.error("Randevu bulunurken hata oluştu")
-            })
-         } else console.log("Zaten randevu alınmış")
-      }).catch(err => console.error("Randevu geçmişi alınırken hata oluştu"))
+   kontrolEt(token, il, cinsiyet, klinik, onumuzdekigun)
+   interval = setInterval(() => {
+      kontrolEt(token, il, cinsiyet, klinik, onumuzdekigun)
    }, 180000)
-}).catch(err => console.error("Giriş başarısız"))
+}).catch(() => console.error("Giriş başarısız"))
